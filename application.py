5bd61c237b0f5aec7f436f3bc52c6d24eb50aa28
oguz.koc <oguz.koc@turkai.com>
2023-05-05 16:07:04 +0300

    05-05-2023

--- application.py
+++ application.py
@@ -1,10 +1,17 @@
+# Import Flask and SQLAlchemy modules
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+# Create a Flask application instance
 app = Flask(__name__)
+
+# Configure the database connection URL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:122333@localhost:5432/task'
+
+# Initialize a SQLAlchemy object
 db = SQLAlchemy(app)
 
+# Define a model for PersonalInformation table
 class PersonalInformation(db.Model):
     __tablename__ = "personal_informations"
     entity_id = db.Column(db.String(20), primary_key=True, nullable=False)
@@ -22,21 +29,27 @@ class PersonalInformation(db.Model):
     is_active = db.Column(db.Boolean, nullable=False)
     thumbnail = db.Column(db.Text)
 
+# Define a route for displaying the list of persons
 @app.route('/results')
 def results():
-    # Kişi listesini veritabanından al
+    # Get the page number from the URL query parameter, or use 1 as default
     page = request.args.get('page', 1, type=int)
+    # Query the database for the list of persons and paginate the results
     persons_query = PersonalInformation.query.order_by(PersonalInformation.name).paginate(page=page, per_page=5)
+    # Extract the items (persons) from the pagination object
     persons = persons_query.items
-    # Sayfalama için gerekli bilgileri oluştur
+    # Generate a list of page numbers for navigation
     pages = range(1, persons_query.pages + 1)
+    # Generate URLs for next and previous pages
     next_url = None
     if persons_query.has_next:
         next_url = f'/results?page={persons_query.next_num}'
     prev_url = None
     if persons_query.has_prev:
         prev_url = f'/results?page={persons_query.prev_num}'
+    # Render the HTML template with the list of persons and pagination information
     return render_template('results.html', persons=persons, pagination=persons_query, pages=pages, next_url=next_url, prev_url=prev_url)
 
+# Run the application if this script is executed as the main program
 if __name__ == '__main__':
     app.run(debug=True)
--- delete_data.py
+++ delete_data.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_creation import (
@@ -9,19 +10,34 @@ from database_creation import (
     LanguageInformation,
 )
 
-engine = create_engine(
-    "postgresql+psycopg2://postgres:122333@localhost:5432/task"
-)
+# Create an engine for the database
+engine = create_engine("postgresql+psycopg2://postgres:122333@localhost:5432/task")
 
+# Create a session maker bound to the engine
 Session = sessionmaker(bind=engine)
+
+# Create a session
 session = Session()
 
-entity_classes = [    LanguageInformation,    NationalityInformation,    ArrestWarrantInformation,    PictureInformation,    ChangeLogInformation,    PersonalInformation,]
+# Define the classes to be deleted
+entity_classes = [
+    LanguageInformation,
+    NationalityInformation,
+    ArrestWarrantInformation,
+    PictureInformation,
+    ChangeLogInformation,
+    PersonalInformation,
+]
 
+# Loop through each class and delete all instances
 for entity_class in entity_classes:
+    # Get all the entity IDs for the class
     entity_ids = session.query(entity_class.entity_id).all()
+    # Extract the entity IDs from the query result
     entity_ids = [entity_id[0] for entity_id in entity_ids]
+    # Delete all the instances with matching entity IDs
     for entity_id in entity_ids:
         session.query(entity_class).filter_by(entity_id=entity_id).delete()
 
+# Commit the changes to the database
 session.commit()
