b52d07c93eb615d4489e1b20be15ae756763d79c
oguz.koc <oguz.koc@turkai.com>
2023-07-11 14:30:16 +0300

    11-07-2023

--- producer.py
+++ producer.py
@@ -1,15 +1,14 @@
 import json
 import os
 import time
-time.sleep(2)
 import pika
 import requests
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-
-from database_creation import PersonalInformation
+from sqlalchemy.exc import IntegrityError
+import datetime
+from database_creation import PersonalInformation, ChangeLogInformation
 from models import InterpolPerson
 
 # Load the .env file
@@ -33,27 +32,51 @@ session = DBSession()
 
 
 class Producer:
-    # Initialize the Producer class
+    """
+    The Producer class for publishing messages to RabbitMQ.
+    """
+
     def __init__(self, key):
+        """
+        Initialize the Producer class.
+
+        Args:
+            key (str): The routing key for the message queue.
+        """
         self.key = key
-        # Create a new connection to the RabbitMQ server
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
-        # Create a new channel on the connection
         self.channel = self.connection.channel()
-        # Declare the name of the queue that will be used to send messages
         self.channel.queue_declare(queue=self.key)
 
-    # Publish a message to the queue
     def publish(self, message):
-        # Use the basic_publish method to send the message to the queue
+        """
+        Publish a message to the RabbitMQ queue.
+
+        Args:
+            message (str): The message to be published.
+        """
         self.channel.basic_publish(exchange='', routing_key=self.key, body=message)
 
-    # Close the connection to the RabbitMQ server
     def close(self):
+        """
+        Close the connection to RabbitMQ.
+        """
         self.connection.close()
 
 
 def perform_request(url):
+    """
+    Perform an HTTP GET request.
+
+    Args:
+        url (str): The URL to make the request.
+
+    Returns:
+        requests.Response: The response object.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs during the request.
+    """
     while True:
         try:
             response = requests.get(url, headers={}, data={})
@@ -62,13 +85,18 @@ def perform_request(url):
             print("Internet connection lost. Trying to reconnect...")
             time.sleep(5)
 
+
 def main():
+    """
+    The main function that performs the data retrieval and database operations.
+    """
+    # Get the first page and retrieve the total number of pages
     list_interpol = "https://ws-public.interpol.int/notices/v1/red?nationality=US&resultPerPage=20&page=1"
     time.sleep(2)
-    # Get the first page and retrieve the total number of pages
     response = perform_request(list_interpol)
     json_list = response.json()
     total_pages = int(json_list['total'] / 20 + 2)
+    entity_id_list = []
 
     for page_num in range(1, total_pages):
         # Construct the page link for each page
@@ -84,6 +112,7 @@ def main():
             interpol_person = InterpolPerson(person_links)
             personal_info_data = interpol_person.get_personal_info_data()
             entity_id = personal_info_data['entity_id']
+            entity_id_list.append(entity_id)
 
             # Check if the person is already in the database
             if session.query(PersonalInformation).filter_by(entity_id=entity_id).first():
@@ -100,9 +129,38 @@ def main():
                 producer.close()
                 print(f"The data with {entity_id} entity_id has been added to the database.")
 
+    # Get the existing entity IDs from the database
+    existing_entity_ids = session.query(PersonalInformation.entity_id).all()
+    existing_entity_ids = [entity_id[0] for entity_id in existing_entity_ids]
+
+    # Update the is_active value for records with missing entity IDs
+    for entity_id in existing_entity_ids:
+        if entity_id not in entity_id_list:
+            person = session.query(PersonalInformation).filter_by(entity_id=entity_id).first()
+            #If the "is_active" property of the person not in the database is "True", set it to "False".
+            if person.is_active == True:
+                person.is_active = False
+                change_log_entry = ChangeLogInformation(
+                    entity_id=entity_id,
+                    table_name="personal_informations",
+                    field_name="is_active",
+                    old_value=True,
+                    new_value=False,
+                    description="Change in personal information",
+                    change_date=datetime.datetime.now()
+                )
+                # Add the ChangeLogInformation object to the session to be committed to the database
+                session.add(change_log_entry)
+
+    # Commit the changes
+    try:
+        session.commit()
+    except IntegrityError:
+        # If there is an integrity error during commit, rollback the transaction
+        session.rollback()
+
 
 if __name__ == "__main__":
-
     while True:
         time.sleep(2)
         main()
