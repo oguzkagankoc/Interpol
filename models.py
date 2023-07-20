b9f0d142a70180b9d968f9040ac9fd667e40bc93
oguz.koc <oguz.koc@turkai.com>
2023-07-20 11:14:57 +0300

    20-07-2023

--- models.py
+++ models.py
@@ -1,8 +1,4 @@
-import base64
 import os
-import time
-
-import requests
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -24,25 +20,6 @@ app.config[
 
 # Initialize a SQLAlchemy object
 db = SQLAlchemy(app)
-
-def perform_request(url):
-    """
-    Performs a GET request to a given URL. If an exception occurs (e.g., due to loss of internet connection),
-    it will retry indefinitely every 5 seconds until successful.
-
-    Parameters:
-    url (str): The URL to perform the GET request.
-
-    Returns:
-    response (requests.Response): The response object containing the server's response to the HTTP request.
-    """
-    while True:
-        try:
-            response = requests.get(url, headers={}, data={})
-            return response
-        except requests.exceptions.RequestException:
-            print("Internet connection lost. Trying to reconnect...")
-            time.sleep(5)
 
 # Define a model for the "personal_informations" table
 class AppPersonalInformation(db.Model):
@@ -134,119 +111,3 @@ class AppArrestWarrantInformation(db.Model):
     personal_informations = db.relationship("AppPersonalInformation", backref="arrest_warrant", lazy=True,
                                             foreign_keys=[entity_id])
 
-
-class InterpolPerson:
-    """
-    This class is responsible for retrieving and processing data about a single criminal from Interpol.
-
-    Attributes:
-    person_url (str): The URL where the data about the criminal is located.
-    personal_info_data (dict): A dictionary to hold the processed data.
-    """
-    def __init__(self, person_url):
-        # Initialize instance variables
-        self.person_url = person_url
-        self.personal_info_data = {}
-
-    def _get_data(self):
-        """
-        This method processes and parses the data from the request,
-        and adds it appropriately to the personal_info_data dictionary.
-        """
-        # Get data from the provided URL
-        response = perform_request(self.person_url)
-        data = response.json()
-
-        # Get the person's image URL, retrieve the image and encode it to base64
-        if "thumbnail" in data['_links'].keys():
-            image_url = data['_links']['thumbnail']['href']
-            image_response = perform_request(image_url)
-            image_content = image_response.content
-            image_base64 = base64.b64encode(image_content).decode("utf-8")
-        else:
-            # If there is no "thumbnail" information, a default image is assigned to the image_base64 value.
-            image_base64 = 'iVBORw0KGgoAAAANSUhEUgAAAKoAAACqAgMAAABAGDwRAAAADFBMVEWutLfk5ufb3d/EyMpaqx/2AAACUUlEQVRYw+3YK3LcQBAA0LZUBgJKkI8gHiLgoD3CAu1IVREQ3wU6gi6xPNQh2QMEzA2CdASDsFCTOJWsVur5dPeM7VTsKgm/mur5dfcIdsHfFla72tWudrVv0359/BFqTwDwEGYP8Oe7D7FK/7XvQux5WM/AHqsnm8u2mSikR9H2FwuFaPVsc8nWM4VUsu1iYRRsj2wpWI1sxluFKCS8rbBNeYunZk/Otp1hC9ZuDFuyVhs2Y61B4YqztWkTzlamTTnbmBaOjG0tOzJ2a9kbxnaWLRjbR9iNZcsIe83Y4Rk2Y6y2bP4fLLyxcV+jzV7oPMScs5iz3j/DFhH3+CYiP4wReecYnvsgIqeyeVJF5GrrQOSsHSLqRR9Rhzp6Kxzb0Mvr2DqiHu/oZXDtQJ10j+3I0u3ahpyaaxXZlnj6s4EK12MPVMvlsTXVyvn61NNkbwNsS7Sp3h785B/Wa/f+Yf3vgL2G98fQ94W6+xfvlv3jB/j+KyRe9W1aswfRqp/zHn8ULKLOEtv2ZNzN+114nkyZPKki6ttnu3YbURi2dqjxMjNs71o8MLbKQ3HE2B58Fl18bLXX3vrsJy9FWQLZzm+Xq4+sJmzm2oagSxCLpUJYgljsQNrStoqk83bMtqFtals63Dng2Q6MLS3L0EvAF1txNjFty1kwbcfa0bADawvDatZm2CqWTgsx2Yq3CbYNb1Nst7w9X9DJdoIdkd0ItkB2EGyJrBbsNbICPW/G2SrJ5outJXv1NFtJNllsI9n0abZ9SQtofb9I3/pPd7Wv0v4Gki3y31ZD0i8AAAAASUVORK5CYII='
-
-        # Save the personal information data in a dictionary
-        self.personal_info_data = {
-            'entity_id': data['entity_id'],
-            'name': data['name'],
-            'forename': data['forename'],
-            'sex_id': data['sex_id'],
-            'country_of_birth_id': data['country_of_birth_id'],
-            'place_of_birth': data['place_of_birth'],
-            'date_of_birth': data['date_of_birth'],
-            'height': data['height'],
-            'eyes_colors_id': None if data['eyes_colors_id'] == None else data['eyes_colors_id'][0],
-            'hairs_id': None if data['hairs_id'] == None else data['hairs_id'][0],
-            'distinguishing_marks': data['distinguishing_marks'],
-            'weight': data['weight'],
-            'is_active': True,
-            'thumbnail': image_base64
-        }
-
-        # Add nationality information to the personal information data
-        nationalities = []
-        if data['nationalities'] is None:
-            self.personal_info_data.update({'nationalities': None})
-        else:
-            for l in data['nationalities']:
-                nationalities.append({
-                    'entity_id': data['entity_id'],
-                    'nationality': l
-                })
-            self.personal_info_data.update({'nationalities': nationalities})
-
-        # Add languages spoken information to the personal information data
-        languages_spoken_ids = []
-        if data['languages_spoken_ids'] is None:
-            self.personal_info_data.update({'languages_spoken_ids': None})
-        else:
-            for l in data['languages_spoken_ids']:
-                languages_spoken_ids.append({
-                    'entity_id': data['entity_id'],
-                    'languages_spoken_id': l
-                })
-            self.personal_info_data.update({'languages_spoken_ids': languages_spoken_ids})
-
-        # Add arrest warrants information to the personal information data
-        arrest_warrants = []
-        if data['arrest_warrants'] is None:
-            self.personal_info_data.update({'arrest_warrants': None})
-        else:
-            for a in data['arrest_warrants']:
-                a.update({'entity_id': data['entity_id']})
-                arrest_warrants.append(a)
-            self.personal_info_data.update({'arrest_warrants': arrest_warrants})
-
-        # Add pictures information to the personal information data
-        pictures = []
-        pictures_link = \
-            perform_request(data['_links']['images']['href']).json()["_embedded"]['images']
-        if pictures_link is None:
-            self.personal_info_data.update({'pictures': None})
-        else:
-            for p in pictures_link:
-                url = p['_links']['self']['href']
-                response = perform_request(url)
-                image_content = response.content
-                image_base64 = base64.b64encode(image_content).decode("utf-8")
-                picture_data = {
-                    'entity_id': data['entity_id'],
-                    'picture_id': p['picture_id'],
-                    'picture_url': p['_links']['self']['href'],
-                    'picture_base64': image_base64
-                }
-                pictures.append(picture_data)
-            self.personal_info_data.update({'pictures': pictures})
-
-    def get_personal_info_data(self):
-        """
-        Retrieves and returns the processed data. If data has not been retrieved and processed yet, it does so first.
-
-        Returns:
-        personal_info_data (dict): The processed data about the criminal.
-        """
-        if not self.personal_info_data:
-            self._get_data()
-        return self.personal_info_data
